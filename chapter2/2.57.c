25e374f3a0b50c4df2dbcd92bc41dc4c06d4e46c
WhiteNight <1448375249@qq.com>
2023-01-15 08:17:05 +0800

    Add 2.55-2.63

---
+++ chapter2/2.57.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+typedef unsigned char* byte_pointer;
+
+void show_bytes(byte_pointer start, size_t len) {
+  size_t i;
+  for (i = 0; i < len; i++) {
+    printf(" %.2x", start[i]);
+  }
+  printf("\n");
+}
+
+void show_int(int x) {
+  show_bytes((byte_pointer) &x, sizeof(int));
+}
+
+void show_float(float x) {
+  show_bytes((byte_pointer) &x, sizeof(float));
+}
+
+void show_pointer(void *x) {
+  show_bytes((byte_pointer) &x, sizeof(void*));
+}
+
+void show_short(short x) {
+  show_bytes((byte_pointer) &x, sizeof(short));
+}
+
+void show_long(long x) {
+  show_bytes((byte_pointer) &x, sizeof(long));
+}
+
+void show_double(double x) {
+  show_bytes((byte_pointer) &x, sizeof(double));
+}
+
+void test_show_bytes(int val) {
+  int ival = val;
+  float fval = (float) ival;
+  int* pval = &ival;
+
+  show_int(ival);
+  show_float(fval);
+  show_pointer(pval);
+
+  short sval = (short)ival;
+  long lval = (long)ival;
+  double dval = (double)ival;
+
+  show_short(sval);
+  show_long(lval);
+  show_double(dval);
+}
+
+int main(int argc, char* argv[]) {
+  int test_num = 328;
+  test_show_bytes(test_num);
+  return 0;
+}
