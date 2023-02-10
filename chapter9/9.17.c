589dd9b9a49bc31423dc455bccc0a02098d07973
White Night <1448375249@qq.com>
2023-02-10 08:09:03 +0800

    Add chapter9

---
+++ chapter9/9.17.c
@@ -0,0 +1,15 @@
+static void *find_fit(size_t asize) {
+  char *oldrover = rover;
+
+  // search from the rover to the end of list
+  for ( ; GET_SIZE(HDRP(rover)) > 0; rover = NEXT_BLKP(rover))
+    if (!GET_ALLOC(HDRP(rover)) && (asize <= GET_SIZE(HDRP(rover))))
+      return rover;
+
+  // search from start of list to old rover
+  for (rover = heap_listp; rover < oldrover; rover = NEXT_BLKP(rover))
+    if (!GET_ALLOC(HDRP(rover)) && (asize <= GET_SIZE(HDRP(rover))))
+      return rover;
+
+  return NULL;
+}
